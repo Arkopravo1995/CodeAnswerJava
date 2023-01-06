277017922c922f9267fc6008f07c8987b09e98d6
Arko <arka.bhuin@gmail>
2023-01-06 20:42:29 +0530

    second commit

---
+++ src/inputTake/TwolineInp.java
@@ -0,0 +1,14 @@
+package inputTake;
+import java.util.*;
+public class TwolineInp {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String inp = sc.nextLine();
+		String inp1 = sc.nextLine();
+		
+		System.out.print(inp + inp1);
+
+	}
+
+}
