22174e916da5bfca93839d914329bd64ce1bcdf2
Arko <arka.bhuin@gmail>
2023-01-14 22:09:10 +0530

    object

--- src/oops/Student.java
+++ src/oops/Student.java
@@ -1,10 +1,14 @@
 package oops;
 
 public class Student {
+	int roll_no = 2;
+	String Student_name ="Jhon";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Student obj = new Student();
+		System.out.println(obj.roll_no);
+		System.out.println(obj.Student_name);
 	}
 
 }
