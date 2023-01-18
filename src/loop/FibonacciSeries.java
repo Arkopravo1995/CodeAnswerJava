1f337283f41b8ef11ce78ff0009697c67428a853
Arko <arka.bhuin@gmail>
2023-01-18 21:13:25 +0530

    refined

--- src/loop/FibonacciSeries.java
+++ src/loop/FibonacciSeries.java
@@ -5,8 +5,8 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
-		int a = 0 ;
-		int b = 1 ;
+		int a = 1 ;
+		int b = 0 ;
 		for(int i =1 ; i<=n ;i++) {
 			int c = a+b ;
 			a = b ;
